c727497bd155cf183f06c09d598cda4267de8641
HyeSeong123 <banggu1997@gmail.com>
2024-12-28 14:26:56 +0900

    구현중

---
+++ app/views/album_organize.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import Canvas
+from PIL import Image, ImageTk
+import time
+
+def organize_photos():
+    print("사진 정리 기능이 실행됐습니다")
--- app/views/main_window.py
+++ app/views/main_window.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout,
 from PyQt5.QtGui import QPixmap, QPalette, QBrush, QFont
 from PyQt5.QtCore import Qt, QPropertyAnimation, QRect
 import tkinter as tk
-
+import album_organize
 import sys
 
 class MainWindow(QMainWindow):
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_stack)
         
         self.main_frame = self.create_main_frame()
-        self.photo_view_frame = self.organize_frame()
+        self.photo_view_frame = self.create_photo_organize_frame()
         
         self.central_stack.addWidget(self.main_frame)
         self.central_stack.addWidget(self.photo_organize_frame)
@@ -28,24 +28,68 @@ class MainWindow(QMainWindow):
         frame = QWidget()
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignCenter)
-        self.add_buttons()
         
-    def set_background(self):
-        background_img = "D:/python_project/test_V2/photo_log/resources/img/album_img.webp"
-        palette = QPalette()
-        pixmap = QPixmap(background_img).scaled(self.width(), self.height(), Qt.KeepAspectRatioByExpanding,Qt.SmoothTransformation)
-        palette.setBrush(QPalette.Background, QBrush(pixmap))
-        self.setPalette(palette)
+        button_texts = ["사진 정리", "사진 보기", "종료"]
+        for text in button_texts:
+            button = QPushButton(text)
+            button.setFont(QFont("Arial",14))
+            button.setStyleSheet("""
+                                    QPushButton(
+                                        border: 2px solid #FFFFFF;
+                                        border-radius: 15px;
+                                        font-size: 16px;
+                                        color: white;
+                                        padding: 10px 20px;
+                                        background-color: rgba(0, 0, 0, 0.6);
+                                    )
+                                    QPushButton:hover{
+                                        background-color: rgba(255, 255, 255, 0.3);
+                                        color: black;
+                                    }
+                                    QPushButton:pressed{
+                                        background-color: rgba(255, 255, 255, 0.5);
+                                    }
+                                 """)
+            button.setCursor(Qt.PointingHandCursor)
+            button.clicked.connect(self.button_action)
+            layout.addWidget(button)
+        
+        frame.setLayout(layout)
+        return frame
     
-    def resizeEvent(self, event):
-        self.set_background()
+    def create_photo_organize_frame(self):
+        frame = QWidget()
+        label = QLabel("앨범 정리")
+        label.setFont(QFont("Arial",20))
+        label.setAlignment(Qt.AlignCenter)
+        
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        frame.setLayout(layout)
+        return frame
         
     def button_action(self):
         sender = self.sender()
         
         if sender.text() == "사진 정리":
-            album_organize();
+            print("사진 보기")
         elif sender.text() == "사진 보기":
             print("사진 보기")
         elif sender.text() == "종료":
             QApplication.quit()
+    
+    def start_photo_organize(self):
+        self.switch_frame(self.photo_organize_frame)
+        
+        album_organize.organize_photos()
+    
+    def switch_frame(self, frame):
+        current_rect = self.central_stack.geometry()
+        target_rect = QRect(current_rect.x() + self.width(), current_rect.y(), self.width(), self.heigth())
+        
+        animation = QPropertyAnimation(self.central_stack, b"geometry")
+        animation.setDuration(500)
+        animation.setStartValue(current_rect)
+        animation.setEndValue(target_rect)
+        animation.finished.connect(lambda: self.central_stack.setCurrentWidget(frame))
+        animation.start()
