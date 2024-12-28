626f58b34f4f742885716846c2bd8bd2dcfc06d1
HyeSeong123 <banggu1997@gmail.com>
2024-12-29 00:36:49 +0900

    구현 중

--- app/views/main_window.py
+++ app/views/main_window.py
@@ -1,11 +1,12 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget, QLabel
-from PyQt5.QtGui import QPixmap, QPalette, QBrush, QFont
+from PyQt5.QtGui import QPixmap, QPalette, QBrush, QFont, QFontDatabase
 from PyQt5.QtCore import Qt, QPropertyAnimation, QRect
 import tkinter as tk
-import album_organize
+from . import album_organize
 import sys
 
 class MainWindow(QMainWindow):
+    QFontDatabase.addApplicationFont("D:/python_project/test_V2/photo_log/resources/font/BMDOHYEON_ttf.ttf")
     def __init__(self):
         super().__init__()
         self.init_ui()
@@ -18,12 +19,28 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_stack)
         
         self.main_frame = self.create_main_frame()
-        self.photo_view_frame = self.create_photo_organize_frame()
+        self.photo_organize_frame = self.create_photo_organize_frame()
         
         self.central_stack.addWidget(self.main_frame)
         self.central_stack.addWidget(self.photo_organize_frame)
         self.central_stack.setCurrentWidget(self.main_frame)
+    
+    def set_background(self):
+        background_img = "D:/python_project/test_V2/photo_log/resources/img/album_img.webp"
         
+        palette = QPalette()
+        pixmap = QPixmap(background_img).scaled(
+            self.width(),
+            self.height(),
+            Qt.KeepAspectRatioByExpanding,
+            Qt.SmoothTransformation
+        )
+        palette.setBrush(QPalette.Background, QBrush(pixmap))
+        self.setPalette(palette)
+        
+    def resizeEvent(self, event):
+        self.set_background()
+    
     def create_main_frame(self):
         frame = QWidget()
         layout = QVBoxLayout()
@@ -32,16 +49,19 @@ class MainWindow(QMainWindow):
         button_texts = ["사진 정리", "사진 보기", "종료"]
         for text in button_texts:
             button = QPushButton(text)
-            button.setFont(QFont("Arial",14))
+            button.setFont(QFont("BMDOHYEON_ttf",14))
             button.setStyleSheet("""
-                                    QPushButton(
+                                    QPushButton{
                                         border: 2px solid #FFFFFF;
+                                        margin-left : 60px;
+                                        margin-bottom : 15px;
                                         border-radius: 15px;
-                                        font-size: 16px;
-                                        color: white;
+                                        font-size: 18px;
+                                        font-weight: bold;
+                                        color: black;
                                         padding: 10px 20px;
-                                        background-color: rgba(0, 0, 0, 0.6);
-                                    )
+                                        background-color: rgba(0, 0, 0, 0);
+                                    }
                                     QPushButton:hover{
                                         background-color: rgba(255, 255, 255, 0.3);
                                         color: black;
@@ -72,7 +92,7 @@ class MainWindow(QMainWindow):
         sender = self.sender()
         
         if sender.text() == "사진 정리":
-            print("사진 보기")
+            self.start_photo_organize()
         elif sender.text() == "사진 보기":
             print("사진 보기")
         elif sender.text() == "종료":
@@ -85,7 +105,7 @@ class MainWindow(QMainWindow):
     
     def switch_frame(self, frame):
         current_rect = self.central_stack.geometry()
-        target_rect = QRect(current_rect.x() + self.width(), current_rect.y(), self.width(), self.heigth())
+        target_rect = QRect(current_rect.x(), current_rect.y(), self.width(), self.height())
         
         animation = QPropertyAnimation(self.central_stack, b"geometry")
         animation.setDuration(500)
